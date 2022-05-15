a68ba318e0ff454448820d9914107f8c72baccc1
Shanur Rahman <shanurrahmanpenny>
2022-05-15 14:53:11 +0530

    added package per app using generatePackageJson flag

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nxkube-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend';
+}
